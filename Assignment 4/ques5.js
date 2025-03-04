0d0c688e7eadcce6e5b63fc1f5df1663e3a99034
Akhil1sharma <168325399+Akhil1sharma@users.noreply.github.com>
2025-03-04 18:25:31 +0530

    Create ques5.js

---
+++ Assignment 4/ques5.js
@@ -0,0 +1,10 @@
+//Write a JavaScript program to find the largest of three given integers.
+
+
+function findLargest(a, b, c) {
+    return Math.max(a, b, c);
+}
+
+// Test cases
+console.log(findLargest(5, 10, 3)); // 10
+console.log(findLargest(-1, -5, -10)); // -1
